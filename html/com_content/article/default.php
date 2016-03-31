8923d309ff5ffbf467735381ca5c16db21a56882
adhocgraFX <info@adhocgrafx.de>
2016-03-31 18:58:15 +0200

    html

--- html/com_content/article/default.php
+++ html/com_content/article/default.php
@@ -24,18 +24,18 @@ $useDefList = ($params->get('show_modify_date') || $params->get('show_publish_da
 
 ?>
 
-<article class="item-page<?php echo $this->pageclass_sfx; ?>" itemscope itemtype="http://schema.org/Article">
+<article class="item-page <?php echo $this->pageclass_sfx; ?>" itemscope itemtype="http://schema.org/Article">
 	<meta itemprop="inLanguage" content="<?php echo ($this->item->language === '*') ? JFactory::getConfig()->get('language') : $this->item->language; ?>" />
 	<?php if ($this->params->get('show_page_heading', 1)) : ?>
-	<div class="page-header">
-		<h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
-	</div>
+		<div class="page-header">
+			<h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
+		</div>
 	<?php endif;
-if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->paginationposition && $this->item->paginationrelative)
-{
-	echo $this->item->pagination;
-}
-?>
+	if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->paginationposition && $this->item->paginationrelative)
+	{
+		echo $this->item->pagination;
+	}
+	?>
 	<?php if (!$useDefList && $this->print) : ?>
 		<div id="pop-print" class="btn btn-blank hidden-print">
 			<?php echo JHtml::_('icon.print_screen', $this->item, $params); ?>
@@ -43,26 +43,26 @@ if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->
 		<div class="clearfix"> </div>
 	<?php endif; ?>
 	<?php if ($params->get('show_title') || $params->get('show_author')) : ?>
-	<div class="page-header">
-		<h1 itemprop="name">
-			<?php if ($params->get('show_title')) : ?>
-				<?php if ($params->get('link_titles') && !empty($this->item->readmore_link)) : ?>
-					<a href="<?php echo $this->item->readmore_link; ?>" itemprop="url"> <?php echo $this->escape($this->item->title); ?></a>
-				<?php else : ?>
-					<?php echo $this->escape($this->item->title); ?>
+		<div class="page-header">
+			<h1 itemprop="name">
+				<?php if ($params->get('show_title')) : ?>
+					<?php if ($params->get('link_titles') && !empty($this->item->readmore_link)) : ?>
+						<a href="<?php echo $this->item->readmore_link; ?>" itemprop="url"> <?php echo $this->escape($this->item->title); ?></a>
+					<?php else : ?>
+						<?php echo $this->escape($this->item->title); ?>
+					<?php endif; ?>
 				<?php endif; ?>
+			</h1>
+			<?php if ($this->item->state == 0) : ?>
+				<span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span>
 			<?php endif; ?>
-		</h1>
-		<?php if ($this->item->state == 0) : ?>
-			<span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span>
-		<?php endif; ?>
-		<?php if (strtotime($this->item->publish_up) > strtotime(JFactory::getDate())) : ?>
-			<span class="label label-warning"><?php echo JText::_('JNOTPUBLISHEDYET'); ?></span>
-		<?php endif; ?>
-		<?php if ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != '0000-00-00 00:00:00') : ?>
-			<span class="label label-warning"><?php echo JText::_('JEXPIRED'); ?></span>
-		<?php endif; ?>
-	</div>
+			<?php if (strtotime($this->item->publish_up) > strtotime(JFactory::getDate())) : ?>
+				<span class="label label-warning"><?php echo JText::_('JNOTPUBLISHEDYET'); ?></span>
+			<?php endif; ?>
+			<?php if ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != '0000-00-00 00:00:00') : ?>
+				<span class="label label-warning"><?php echo JText::_('JEXPIRED'); ?></span>
+			<?php endif; ?>
+		</div>
 	<?php endif; ?>
 	<?php if (!$this->print) : ?>
 		<?php if ($canEdit || $params->get('show_print_icon') || $params->get('show_email_icon')) : ?>
@@ -76,9 +76,9 @@ if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->
 		<?php endif; ?>
 	<?php endif; ?>
 
-    <?php if ($useDefList && ($info == 0 || $info == 2)) : ?>
-        <?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'position' => 'above')); ?>
-    <?php endif; ?>
+	<?php if ($useDefList && ($info == 0 || $info == 2)) : ?>
+		<?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'position' => 'above')); ?>
+	<?php endif; ?>
 
 	<?php if ($info == 0 && $params->get('show_tags', 1) && !empty($this->item->tags->itemTags)) : ?>
 		<?php $this->item->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
@@ -91,71 +91,71 @@ if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->
 
 	<?php if (isset($urls) && ((!empty($urls->urls_position) && ($urls->urls_position == '0')) || ($params->get('urls_position') == '0' && empty($urls->urls_position)))
 		|| (empty($urls->urls_position) && (!$params->get('urls_position')))) : ?>
-	<?php echo $this->loadTemplate('links'); ?>
+		<?php echo $this->loadTemplate('links'); ?>
 	<?php endif; ?>
 	<?php if ($params->get('access-view')):?>
-	<?php if (isset($images->image_fulltext) && !empty($images->image_fulltext)) : ?>
-	<?php $imgfloat = (empty($images->float_fulltext)) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
-	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
-	<?php if ($images->image_fulltext_caption):
-		echo 'class="caption"'.' title="' .htmlspecialchars($images->image_fulltext_caption) . '"';
-	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>" itemprop="image"/> </div>
-	<?php endif; ?>
-	<?php
-	if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->paginationposition && !$this->item->paginationrelative):
-		echo $this->item->pagination;
-	endif;
-	?>
-	<?php if (isset ($this->item->toc)) :
-		echo $this->item->toc;
-	endif; ?>
-	<div itemprop="articleBody">
-		<?php echo $this->item->text; ?>
-	</div>
-
-    <?php if ($useDefList && ($info == 1 || $info == 2)) : ?>
-            <?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'position' => 'below')); ?>
-        <?php  endif; ?>
-
-	<?php if (!empty($this->item->pagination) && $this->item->pagination && $this->item->paginationposition && !$this->item->paginationrelative):
-	    echo $this->item->pagination; ?>
-	<?php endif; ?>
-	<?php if (isset($urls) && ((!empty($urls->urls_position) && ($urls->urls_position == '1')) || ($params->get('urls_position') == '1'))) : ?>
-	<?php echo $this->loadTemplate('links'); ?>
-	<?php endif; ?>
-	<?php // Optional teaser intro text for guests ?>
-	<?php elseif ($params->get('show_noauth') == true && $user->get('guest')) : ?>
-	<?php echo $this->item->introtext; ?>
-	<?php //Optional link to let them register to see the whole article. ?>
-	<?php if ($params->get('show_readmore') && $this->item->fulltext != null) :
-		$link1 = JRoute::_('index.php?option=com_users&view=login');
-		$link = new JUri($link1);?>
-	<p class="readmore">
-		<a href="<?php echo $link; ?>">
-		<?php $attribs = json_decode($this->item->attribs); ?>
+		<?php if (isset($images->image_fulltext) && !empty($images->image_fulltext)) : ?>
+			<?php $imgfloat = (empty($images->float_fulltext)) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
+			<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
+					<?php if ($images->image_fulltext_caption):
+						echo 'class="caption"'.' title="' .htmlspecialchars($images->image_fulltext_caption) . '"';
+					endif; ?>
+					src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>" itemprop="image"/> </div>
+		<?php endif; ?>
 		<?php
-		if ($attribs->alternative_readmore == null) :
-			echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE');
-		elseif ($readmore = $this->item->alternative_readmore) :
-			echo $readmore;
-			if ($params->get('show_readmore_title', 0) != 0) :
-				echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
-			endif;
-		elseif ($params->get('show_readmore_title', 0) == 0) :
-			echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE');
-		else :
-			echo JText::_('COM_CONTENT_READ_MORE');
-			echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
+		if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->paginationposition && !$this->item->paginationrelative):
+			echo $this->item->pagination;
+		endif;
+		?>
+		<?php if (isset ($this->item->toc)) :
+			echo $this->item->toc;
 		endif; ?>
-		</a>
-	</p>
-	<?php endif; ?>
+		<div itemprop="articleBody">
+			<?php echo $this->item->text; ?>
+		</div>
+
+		<?php if ($useDefList && ($info == 1 || $info == 2)) : ?>
+			<?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'position' => 'below')); ?>
+		<?php  endif; ?>
+
+		<?php if (!empty($this->item->pagination) && $this->item->pagination && $this->item->paginationposition && !$this->item->paginationrelative):
+			echo $this->item->pagination; ?>
+		<?php endif; ?>
+		<?php if (isset($urls) && ((!empty($urls->urls_position) && ($urls->urls_position == '1')) || ($params->get('urls_position') == '1'))) : ?>
+			<?php echo $this->loadTemplate('links'); ?>
+		<?php endif; ?>
+		<?php // Optional teaser intro text for guests ?>
+	<?php elseif ($params->get('show_noauth') == true && $user->get('guest')) : ?>
+		<?php echo $this->item->introtext; ?>
+		<?php //Optional link to let them register to see the whole article. ?>
+		<?php if ($params->get('show_readmore') && $this->item->fulltext != null) :
+			$link1 = JRoute::_('index.php?option=com_users&view=login');
+			$link = new JUri($link1);?>
+			<p class="readmore">
+				<a href="<?php echo $link; ?>">
+					<?php $attribs = json_decode($this->item->attribs); ?>
+					<?php
+					if ($attribs->alternative_readmore == null) :
+						echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE');
+					elseif ($readmore = $this->item->alternative_readmore) :
+						echo $readmore;
+						if ($params->get('show_readmore_title', 0) != 0) :
+							echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
+						endif;
+					elseif ($params->get('show_readmore_title', 0) == 0) :
+						echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE');
+					else :
+						echo JText::_('COM_CONTENT_READ_MORE');
+						echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
+					endif; ?>
+				</a>
+			</p>
+		<?php endif; ?>
 	<?php endif; ?>
 	<?php
-if (!empty($this->item->pagination) && $this->item->pagination && $this->item->paginationposition && $this->item->paginationrelative) :
-	echo $this->item->pagination;
-?>
+	if (!empty($this->item->pagination) && $this->item->pagination && $this->item->paginationposition && $this->item->paginationrelative) :
+		echo $this->item->pagination;
+		?>
 	<?php endif; ?>
 	<?php echo $this->item->event->afterDisplayContent; ?>
 </article>
